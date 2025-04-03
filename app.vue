1f7236dbc123c2560fa4b6ca4eb306649a4cb85b
Mehran Mahdavi <mahdavi2it@gmail.com>
2025-04-03 18:02:16 +0330

    feat: add pages

--- app.vue
+++ app.vue
@@ -1,6 +1,18 @@
+<script setup>
+useHead({
+  title: 'Mamad Electronic Shop',
+  meta: [
+    { name: 'description', content: 'Your premier electronics shop' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+  ]
+})
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div data-theme="light" class="h-screen">
+    <NuxtPage />
   </div>
 </template>
