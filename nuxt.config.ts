61855b8a6b509dd392c892c24d93c242499ae5fc
Master2iT <mahdavi2it@gmail.com>
2025-08-04 18:51:52 +0330

    Update nuxt.config.ts description to 'Pars Premium' and modify package.json preview script to include port 3003.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,7 +33,7 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       meta: [
-        { name: 'description', content: 'Mamad Electronic' },
+        { name: 'description', content: 'Pars Premium' },
         { name: 'theme-color', content: '#ffffff' },
       ],
       link: [
