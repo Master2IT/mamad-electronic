655591d4d018fc6b754d5e3f9e080e02a4d107a6
Mehran Mahdavi <mahdavi2it@gmail.com>
2025-08-19 11:46:48 +0330

    Refactor Header and MegaMenu components to enhance layout and functionality. Improve styling consistency, add subcategory navigation in MegaMenu, and adjust search input handling in Header.

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,21 @@
 <template>
-  <header class="border-b border-gray-200 !bg-white px-4 mb-5">
+  <header class="mb-5 border-b border-gray-200 !bg-white px-4">
     <UContainer>
-      <div class="flex flex-col items-center justify-between gap-4 py-4 md:flex-row">
+      <div
+        class="flex flex-col items-center justify-between gap-4 py-4 md:flex-row"
+      >
         <!-- Logo and Brand -->
         <nuxt-link external to="/">
           <div class="flex items-center justify-center md:justify-start">
-            <NuxtImg src="logo.svg" alt="logo" class="ml-2 w-[60px] md:w-[81px]" />
+            <NuxtImg
+              src="logo.svg"
+              alt="logo"
+              class="ml-2 w-[60px] md:w-[81px]"
+            />
             <div>
-              <p class="text-secondary-300 text-xl leading-6 font-bold md:text-2xl lg:text-4xl">
+              <p
+                class="text-secondary-300 text-xl leading-6 font-bold md:text-2xl lg:text-4xl"
+              >
                 محمد
               </p>
               <span class="text-sm md:text-base">الکترونیک</span>
@@ -17,21 +25,47 @@
 
         <!-- Search Bar -->
         <div class="order-3 flex w-full gap-2 md:order-2 md:w-auto">
-          <UInput placeholder="جستجو" icon="i-heroicons-magnifying-glass" class="w-full md:w-[300px] lg:w-[500px]"
-            size="xl" @keydown.enter="goToSearch" />
+          <UInput
+            placeholder="جستجو"
+            icon="i-heroicons-magnifying-glass"
+            class="w-full md:w-[300px] lg:w-[500px]"
+            size="xl"
+            @keydown.enter="goToSearch"
+          />
         </div>
 
         <!-- User Actions -->
-        <div class="order-2 hidden w-full md:order-3 md:block md:w-auto md:space-y-4">
+        <div
+          class="order-2 hidden w-full md:order-3 md:block md:w-auto md:space-y-4"
+        >
           <div class="flex items-center justify-end gap-4 md:hidden">
-            <UButton variant="ghost" icon="i-lucide-shopping-cart" class="size-8 md:size-10" />
-            <UButton color="primary" icon="i-lucide-log-in" size="sm" class="text-sm">
+            <UButton
+              variant="ghost"
+              icon="i-lucide-shopping-cart"
+              class="size-8 md:size-10"
+            />
+            <UButton
+              color="primary"
+              icon="i-lucide-log-in"
+              size="sm"
+              class="text-sm"
+            >
               ورود
             </UButton>
           </div>
           <div class="hidden items-center justify-between gap-4 md:flex">
-            <UModal close v-model:open="isLoginModalOpen" @update:open="authStore.setLoginModal" v-if="!hasToken">
-              <UButton color="primary" icon="i-lucide-log-in" size="lg" class="text-sm md:text-base">
+            <UModal
+              close
+              v-model:open="isLoginModalOpen"
+              @update:open="authStore.setLoginModal"
+              v-if="!hasToken"
+            >
+              <UButton
+                color="primary"
+                icon="i-lucide-log-in"
+                size="lg"
+                class="text-sm md:text-base"
+              >
                 ورود / ثبت نام
               </UButton>
               <template #content>
@@ -39,11 +73,23 @@
                 <AuthVerifyForm v-else />
               </template>
             </UModal>
-            <UButton v-else to="/panel/profile" color="secondary" icon="i-lucide-log-out" size="lg"
-              class="text-sm md:text-base">
+            <UButton
+              v-else
+              to="/panel/profile"
+              color="secondary"
+              icon="i-lucide-log-out"
+              size="lg"
+              class="text-sm md:text-base"
+            >
               ورود به پنل
             </UButton>
-            <UButton icon="i-lucide-shopping-cart" size="lg" color="primary" variant="ghost" @click="goToCard" />
+            <UButton
+              icon="i-lucide-shopping-cart"
+              size="lg"
+              color="primary"
+              variant="ghost"
+              @click="goToCard"
+            />
           </div>
           <div class="flex justify-between text-xs sm:text-sm md:text-base">
             <p>پشتیبانی</p>
@@ -77,7 +123,7 @@ const isCodeSent = computed(() => {
 })
 const { data: categories } = await useAsyncData('categories', async () => {
   const categories = await getCategories()
-  console.log("categories: ", categories[0].children)
+  console.log('categories: ', categories)
   return categories.map((category) => ({
     id: category.id,
     label: category.title,
@@ -97,6 +143,18 @@ const { data: categories } = await useAsyncData('categories', async () => {
         label: subChild.title,
         to: `/category/${subChild?.slug || subChild.id}`,
         status: subChild.status,
+        children: subChild?.children?.map((subSubChild) => ({
+          id: subSubChild.id,
+          label: subSubChild.title,
+          to: `/category/${subSubChild?.slug || subSubChild.id}`,
+          status: subSubChild.status,
+          children: subSubChild?.children?.map((subSubSubChild) => ({
+            id: subSubSubChild.id,
+            label: subSubSubChild.title,
+            to: `/category/${subSubSubChild?.slug || subSubSubChild.id}`,
+            status: subSubSubChild.status,
+          })),
+        })),
       })),
     })),
   }))
--- components/common/MegaMenu.vue
+++ components/common/MegaMenu.vue
@@ -6,7 +6,12 @@
     @mouseenter="openMenu(category.id)"
     @mouseleave="closeMenu(category.id)"
   >
-    <UButton :key="category.id" v-if="category.children?.length && category.show_in_menu" variant="ghost" class="flex items-center gap-2">
+    <UButton
+      :key="category.id"
+      v-if="category.children?.length && category.show_in_menu"
+      variant="ghost"
+      class="flex items-center gap-2"
+    >
       <span class="text-[13px]">{{ category.label }}</span>
       <span>
         <UIcon name="i-lucide-chevron-down" class="size-4 align-middle" />
@@ -22,11 +27,11 @@
 
     <div
       v-if="openMenus[category.id] && category.children?.length"
-      class="absolute top-full right-0 z-50 w-[1200px] rounded-lg border border-neutral-100 bg-white p-4 mt-1 shadow-lg"
+      class="absolute top-full right-0 z-50 mt-1 w-[1200px] rounded-lg border border-neutral-100 bg-white p-4 shadow-lg"
     >
       <div class="flex">
         <!-- Parent Categories Column -->
-        <div class="w-[200px] border-l pl-3 border-neutral-300">
+        <div class="min-w-[300px] border-l border-neutral-300 pl-3">
           <UButton
             :to="child.to"
             external
@@ -34,24 +39,88 @@
             v-for="child in category.children"
             :key="child.id"
             @mouseenter="activeCategory = child"
-            class="hover:bg-primary-100 flex cursor-pointer items-center justify-between gap-2 rounded-lg p-2 transition-colors"
+            class="hover:bg-transparent text-neutral-800 hover:text-primary-500 flex cursor-pointer items-center justify-between gap-2 rounded-lg p-2 transition-colors"
           >
-            <h3 class="text-sm font-medium text-neutral-800">{{ child.label }}</h3>
-            <UIcon v-if="child.children?.length" name="i-lucide-chevron-right" class="size-5 text-gray-400" />
+            <h3 class="text-sm font-medium">
+              {{ child.label }}
+            </h3>
+            <UIcon
+              v-if="child.children?.length"
+              name="i-lucide-chevron-left"
+              class="size-5 text-gray-400"
+            />
           </UButton>
         </div>
 
         <!-- Subcategories Column -->
-        <div v-if="activeCategory && activeCategory?.children?.length" class="w-2/3 p-4">
+        <div
+          v-if="activeCategory && activeCategory?.children?.length"
+          class="min-w-[300px] p-4 border-l border-neutral-300 pl-3 bg-primary-100"
+        >
           <ul class="space-y-2">
             <li v-for="sub in activeCategory.children" :key="sub.id">
               <UButton
                 :to="sub.to"
                 external
                 variant="ghost"
-                class="hover:text-primary-500 w-full text-start text-sm text-gray-600 transition-colors"
+                @mouseenter="activeSubCategory = sub"
+                class="hover:text-primary-500 hover:bg-transparent flex justify-between items-center w-full text-start text-sm text-gray-600 transition-colors"
               >
                 {{ sub.label }}
+                <UIcon
+                  v-if="sub.children?.length"
+                  name="i-lucide-chevron-left"
+                  class="size-5 text-gray-400"
+                />
+              </UButton>
+            </li>
+          </ul>
+        </div>
+
+        <!-- Sub-Subcategories Column -->
+        <div
+          v-if="activeSubCategory && activeSubCategory?.children?.length"
+          class="min-w-[300px] p-4 border-l border-neutral-300 pl-3"
+        >
+          <ul class="space-y-2">
+            <li v-for="sub in activeSubCategory.children" :key="sub.id">
+              <UButton
+                :to="sub.to"
+                external
+                variant="ghost"
+                @mouseenter="activeSubSubCategory = sub"
+                class="hover:text-primary-500 flex justify-between items-center hover:bg-transparent w-full text-start text-sm text-gray-600 transition-colors"
+              >
+                {{ sub.label }}
+                <UIcon
+                  v-if="sub.children?.length"
+                  name="i-lucide-chevron-left"
+                  class="size-5 text-gray-400"
+                />
+              </UButton>
+            </li>
+          </ul>
+        </div>
+
+        <!-- Sub-Sub-Subcategories Column -->
+        <div
+          v-if="activeSubSubCategory && activeSubSubCategory?.children?.length"
+          class="min-w-[300px] p-4"
+        >
+          <ul class="space-y-2">
+            <li v-for="sub in activeSubSubCategory.children" :key="sub.id">
+              <UButton
+                :to="sub.to"
+                external
+                variant="ghost"
+                class="hover:text-primary-500 flex justify-between items-center hover:bg-transparent w-full text-start text-sm text-gray-600 transition-colors"
+              >
+                {{ sub.label }}
+                <UIcon
+                  v-if="sub.children?.length"
+                  name="i-lucide-chevron-left"
+                  class="size-5 text-gray-400"
+                />
               </UButton>
             </li>
           </ul>
@@ -64,6 +133,8 @@
 <script setup>
 const openMenus = ref({})
 const activeCategory = ref(null)
+const activeSubCategory = ref(null)
+const activeSubSubCategory = ref(null)
 let closeTimeouts = {}
 
 const props = defineProps({
@@ -73,12 +144,17 @@ const props = defineProps({
   },
 })
 
+watch(activeSubCategory, (newVal) => {
+  console.log('newVal: ', newVal)
+})
+
 const openMenu = (categoryId) => {
   if (closeTimeouts[categoryId]) {
     clearTimeout(closeTimeouts[categoryId])
+    closeTimeouts[categoryId] = null
   }
   openMenus.value[categoryId] = true
-  const category = props.categories.find(cat => cat.id === categoryId)
+  const category = props.categories.find((cat) => cat.id === categoryId)
   activeCategory.value = category?.children?.[0] || null
 }
 
@@ -86,6 +162,15 @@ const closeMenu = (categoryId) => {
   closeTimeouts[categoryId] = setTimeout(() => {
     openMenus.value[categoryId] = false
     activeCategory.value = null
-  }, 50)
+    activeSubCategory.value = null
+    activeSubSubCategory.value = null
+  }, 250)
+}
+
+const cancelClose = (categoryId) => {
+  if (closeTimeouts[categoryId]) {
+    clearTimeout(closeTimeouts[categoryId])
+    closeTimeouts[categoryId] = null
+  }
 }
 </script>
