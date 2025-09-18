a2fa954a1b4d04bea8d258d0608dbb65ee875e6b
Hamid369 <hamidrezakhani347@gmail.com>
2025-09-18 12:48:01 +0330

    Refactor customer API call in getFavorite function to remove console log. Update ProductCard.vue to replace Heart icon with SVG and improve layout structure. Enhance WishList.vue by adding loading state and adjusting grid layout for better responsiveness.

--- components/panel/WishList.vue
+++ components/panel/WishList.vue
@@ -1,34 +1,42 @@
 <template>
-<div class="flex flex-col gap-5 p-5">
+  <div class="flex w-full flex-col gap-5 p-5">
     <div>
-        <h1 class="text-2xl font-bold mb-1">علاقه مندی ها</h1>
-        <p class="text-gray-600">مواردی که لیست شده</p>
+      <h1 class="text-2xl font-bold mb-1">علاقه مندی ها</h1>
+      <p class="text-gray-600">مواردی که لیست شده</p>
     </div>
-    <div class="grid grid-cols-4 gap-4">
-        <CommonProductCard 
-            v-for="product in data?.items" 
-            :key="product.id"
-            :type="2" 
-            :product="{item}"
-            show-review 
-        />
+    <div class="grid grid-cols-1 sm:grid-cols-4 gap-4 ">
+      <template v-if="loading">
+        <div v-for="i in 8" :key="i" class="animate-pulse">
+          <div class="bg-gray-200 rounded-md h-[350px]  mb-3">
+            <div class="p-4 w-full">
+              <div class="bg-gray-300 h-[200px] w-full rounded-md mb-4"></div>
+              <div class="bg-gray-300 h-4 w-3/4 rounded mb-2"></div>
+              <div class="bg-gray-300 h-3 w-1/2 rounded mb-3"></div>
+              <div class="bg-gray-300 h-8 w-full rounded"></div>
+            </div>
+          </div>
+        </div>
+      </template>
+      <template v-else>
+        <CommonProductCard v-for="product in favorites" :key="product.id" :type="2" :product="product" show-review />
+      </template>
     </div>
-</div>
+  </div>
 </template>
 
 <script setup>
 import { getFavorite } from '~/api/customer-api';
-
-const data = ref(null)
+const favorites = ref([])
+const loading = ref(true)
 
 onMounted(async () => {
+  loading.value = true
   try {
-    const response = await getFavorite()
-    data.value = response
-    console.log("data", data.value)
+    favorites.value = await getFavorite()
   } catch (error) {
-    console.error("Error fetching favorites:", error)
+    console.error('Error fetching favorites:', error)
+  } finally {
+    loading.value = false
   }
 })
-
 </script>
